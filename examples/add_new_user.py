297d0bbdddb5871bb312ae01a408670e93f28907
itsakmak <xylopodaros@yahoo.gr>
2025-08-02 13:03:25 +0300

    updating ingore list

--- examples/add_new_user.py
+++
@@ -1,5 +0,0 @@
-from cordex_database import crud, schemas
-
-new_user = schemas.UsersBaseCreate(aws_user_name='4aaac506-6eb0-4db7-a60e-6077b4f9189d',email='admin@envrio.org',account_type='academic',subscription_expires_in='2100-12-31T23:55:00')
-
-crud.User.add(new_user=new_user)
